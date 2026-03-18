65c881ce2808c2227d7da1a36bcab87c7bc25989
s0nnarr <s0narr@protonmail.com>
2026-03-18 10:10:01 +0200

    First working prototype of AiSight. Added classes: Oreo, Chio and Pepsi. Train evaluation available in runs->detect->train5

--- app/detector.py
+++
@@ -1,4 +0,0 @@
-'''
-    Will be later used to use the model on the video_stream's frames.
-
-'''
--- scripts/train.py
+++ scripts/train.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo26m.pt")
-
-model.train(
-    # experiment with hyperparameters
-    data="dataset.yaml",
-    epochs=50,
-    imgsz=640,
-    batch=16
-)
-
+def main():
+    model = YOLO("yolo26m.pt")
+    model.train(
+        data="E:/yolo_dataset/data.yaml",
+        epochs=100,
+        imgsz=640,
+        batch=8,
+        workers=4
+    )
+if __name__ == "__main__":
+    main()
