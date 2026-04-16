9e39208b0827102d5d895b5686f94b5f6958039f
s0nnarr <s0narr@protonmail.com>
2026-04-16 11:35:10 +0300

    Added Docker setup, modified the train script to better suit a stronger GPU for the first 3 classes training.

--- scripts/train.py
+++ scripts/train.py
@@ -1,13 +1,34 @@
 from ultralytics import YOLO
+import os
+from roboflow import Roboflow
+from huggingface_hub import hf_hub_download
+
+
+
+# Download the Huggingface model
+
 
 def main():
-    model = YOLO("sku_baseline.pt")
+    # model = YOLO("sku_baseline.pt")
+    rf = Roboflow(api_key=os.environ.get("ROBOFLOW_API_KEY"))
+    rf_workspace = rf.workspace("stancus-workspace").project("my-first-project-uj5as")
+    dataset = rf_workspace.version(1).download("yolov8")
+
+    model_path = hf_hub_download(
+        repo_id="s0narr/aisight-model",
+        filename="best.pt",
+        token=os.environ.get("HF_ACCESS_TOKEN")
+    )
+
+    model = YOLO(model_path)
+
     model.train(
-        data="sku_local.yaml",
-        epochs=8,
+        data=f"{dataset.location}/data.yaml",
+        epochs=100,
         imgsz=640,
-        batch=8,
-        workers=4
+        batch=64,
+        workers=4,
+        patience=15
     )
 if __name__ == "__main__":
     main()
